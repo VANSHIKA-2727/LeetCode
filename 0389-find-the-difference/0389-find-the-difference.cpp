20f8e99ac7bee62f9d840d5c22fca338f378af78
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-07-22 01:25:00 +0530

    Time: 0 ms (100%), Space: 9.7 MB (10.53%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char ,int> m; 
-        int i=0, j=0 ;
-        int n = s.size();
-        int y = t.size();
-        while(i<n && j<y ){
-            m[s[i]]++ ;
-            m[t[j]]-- ;
-            i++ ; j++ ;
-        }
+        // unordered_map<char ,int> m; 
+        // int i=0, j=0 ;
+        // int n = s.size();
+        // int y = t.size();
+        // while(i<n && j<y ){
+        //     m[s[i]]++ ;
+        //     m[t[j]]-- ;
+        //     i++ ; j++ ;
+        // }
 
-        while(i<n){
-            m[s[i]]++ ; i++ ;
-        }
+        // while(i<n){
+        //     m[s[i]]++ ; i++ ;
+        // }
 
-        while(j<y){
-            m[t[j]]-- ; j++ ;
-        }
+        // while(j<y){
+        //     m[t[j]]-- ; j++ ;
+        // }
         
-        for(auto x=m.begin() ; x!=m.end();x++){
-            if(x->second!=0){
-                return x->first ;
-            }
-        }
-        return ' ';
+        // for(auto x=m.begin() ; x!=m.end();x++){
+        //     if(x->second!=0){
+        //         return x->first ;
+        //     }
+        // }
+        // return ' ';
+
+    s=s+t ;
+    char a=s[0];
+    for(int i=1 ;i<s.length();i++){
+        a=a^s[i];
+    }
+ return a;
     }
 };
