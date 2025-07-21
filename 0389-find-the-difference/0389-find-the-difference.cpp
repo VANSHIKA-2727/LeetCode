1e27d5311e3f31bf8f70697a2a0f59ba0fda3d29
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-07-22 01:19:04 +0530

    Time: 3 ms (15.71%), Space: 9.2 MB (78.22%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char ,int> m; 
+        int i=0, j=0 ;
+        int n = s.size();
+        int y = t.size();
+        while(i<n && j<y ){
+            m[s[i]]++ ;
+            m[t[j]]-- ;
+            i++ ; j++ ;
+        }
+
+        while(i<n){
+            m[s[i]]++ ; i++ ;
+        }
+
+        while(j<y){
+            m[t[j]]-- ; j++ ;
+        }
+        
+        for(auto x=m.begin() ; x!=m.end();x++){
+            if(x->second!=0){
+                return x->first ;
+            }
+        }
+        return ' ';
+    }
+};
