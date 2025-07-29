8c8ddd6f95690f5f3ffbe90a7ac19edf007134e0
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-07-29 23:58:18 +0530

    Time: 3 ms (2.92%), Space: 8.7 MB (12.04%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int rem ;
+        vector<int>v ;
+        while(n>0){
+        rem = n%2 ;
+        v.push_back(rem );
+        n=n/2 ;}
+        
+        int c =0 ;
+
+        for (int i=0 ;i<v.size();i++){
+            if (v[i]!=0){
+                c++ ;
+            }
+        }
+
+        return c ; 
+    }
+};
