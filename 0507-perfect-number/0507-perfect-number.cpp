ad95d75e4fb83e3d6e7846f1ebded418b637e986
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-08-20 03:33:52 +0530

    Time: 2072 ms (32.11%), Space: 8.1 MB (5.89%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        vector<int>v ;
+        int sum =0 ;
+        for(int i=1;i<num ;i++){
+            if(num%i==0){
+                v.push_back(i);
+            }
+        }
+
+        for(int i=0 ;i<v.size();i++){
+            sum = sum+v[i];
+        }
+
+        if(sum== num){
+            return true ;
+        }
+        return false ;
+    }
+};
