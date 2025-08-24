e34f80da671bf64edb88599e2584d7973f283caf
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-08-24 22:30:56 +0530

    Time: 0 ms (100%), Space: 8 MB (44.17%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+      int c=0;
+      while(num>0)
+      {
+        c++;
+        if(num%2==0)
+        num=num/2;
+        else
+        num--;
+      }
+      return c;
+    }
+};
