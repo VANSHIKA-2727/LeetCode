ff5dbf9a39592d389663a230c21705c0e4016481
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-09-26 00:35:12 +0530

    Time: 0 ms (100%), Space: 9.3 MB (94.28%) - LeetHub

--- 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.cpp
+++ 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> evenOddBit(int n) {
         int e =0 ,o=0 ;
-        int c=0 ;
+        int c=0 ; // number chopping (always from back)
         while(n>0){
-            if(c%2==0 && (n&1)==1)e++;
-            else if ( c%2 !=0 && (n&1)==1)o++;
+            if(c%2==0 && (n&1)==1)e++; 
+            else if ( c%2 !=0 && (n&1)==1)o++; 
         
         n=n>>1 ;
         c++ ;}
