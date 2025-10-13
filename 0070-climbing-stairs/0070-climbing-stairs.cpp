19e407cef2a570ae8c49b8a071eb2d412409f401
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-10-13 12:49:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
+// fibonacci series ;
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        vector<int>v (n+1);
+        v[0]=1 ;
+        v[1]=1 ;
+        for(int i=2 ; i<=n; i++){
+            v[i]=v[i-1]+v[i-2];
         }
 
-        vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];
+       return v[n];
     }
 };
