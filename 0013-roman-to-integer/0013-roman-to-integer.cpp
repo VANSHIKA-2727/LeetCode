ff97bebbd479066544e2f7f161c80fccad0e81f1
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-12-17 13:11:03 +0530

    Time: 3 ms (74.89%), Space: 10 MB (93.69%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int check (char c){
+    if(c=='I')return 1 ;
+    if(c=='V')return 5 ;
+    if(c=='X')return 10 ;
+    if(c=='L')return 50 ;
+    if(c=='C')return 100 ;
+    if(c=='D')return 500 ;
+    else 
+    return 1000 ;
+
+}
+    int romanToInt(string s) {
+
+        int ss =0 ;
+        for (int i = 0 ;i<s.length();i++){
+            if(i< s.length()-1  && check (s[i])< check (s[i+1])){
+                ss= ss-check(s[i]);
+            }
+            else{
+                ss= ss+check(s[i]);
+            }
+        }
+        return ss ;
+
+    }
+};
