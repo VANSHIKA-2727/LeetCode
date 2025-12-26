b703bf35961ab4dd3bdf1f506fec8d461fb6a2a7
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-12-26 13:45:18 +0530

    Time: 17 ms (7.37%), Space: 21.6 MB (5.11%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       int c=0;
-       vector<int>v;
-       v.push_back(nums[0]);
-       c=1;
-       for(int i=1;i<nums.size();i++)
-       {
+    //    int c=0;
+    //    vector<int>v;
+    //    v.push_back(nums[0]);
+    //    c=1;
+    //    for(int i=1;i<nums.size();i++)
+    //    {
         
-        if(nums[i-1]!=nums[i])
-        {
-            c=1;
-        }
-        else 
-        c++;
-        if(c<=2)
-            v.push_back(nums[i]);
+    //     if(nums[i-1]!=nums[i])
+    //     {
+    //         c=1;
+    //     }
+    //     else 
+    //     c++;
+    //     if(c<=2)
+    //         v.push_back(nums[i]);
 
-       }
+    //    }
+
+    map<int,int>m ;
+    for(int i=0 ;i<nums.size();i++){
+        m[nums[i]]++;
+    }
+    vector<int>v ;
+    for(auto x:m){
+        if(x.second>1){
+            v.push_back(x.first);
+             v.push_back(x.first);
+        }
+        else{
+             v.push_back(x.first);
+        }
+    }
+
        int i=0;
-       for(int x:v)
-       nums[i++]=x;
+       for(int y:v)
+       nums[i++]=y;
       return i;
     }
 };
