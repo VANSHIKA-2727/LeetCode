35a0cd3ecf5deea6c0a502c71ce8a2ad6b029ba6
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-12-30 14:01:01 +0530

    Time: 0 ms (100%), Space: 27.3 MB (28.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int trap(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>l(n);
-        vector<int>r(n) ;
-        l[0]=nums[0];
-        r[n-1]=nums[n-1];
-
-        int res =0 ;
-
-        for (int i =1 ;i<n;i++){
-            l[i]=max(nums[i],l[i-1]);
+    int trap(vector<int>& height) {
+        int n =height.size();
+        //l->r
+        vector<int>v1(n,0);
+        v1[0]=height[0];
+        int maxi ;
+        for(int i=1 ;i<height.size();i++){
+            v1[i]=max(v1[i-1],height[i]);          
         }
+        //r->l
+        vector<int>v2(n,0);
+        v2[n-1]=height[n-1];
+         for(int i=n-2 ;i>=0;i--){
+            v2[i]=max(v2[i+1],height[i]);
+     }
+    int s=0 ; 
+    for(int i=0 ;i<height.size();i++){
+    s=s+(min(v1[i],v2[i])-height[i]);
 
-        for(int i=n-2 ;i>=0;i--){
-            r[i]=max(nums[i],r[i+1]);
-        }
-
-        for(int i=0 ;i<n ;i++){
-         res = res+(min(l[i],r[i])-nums[i]) ;
-        }
-
-        return res ;
-
+    }
+    return s ;
     }
 };
