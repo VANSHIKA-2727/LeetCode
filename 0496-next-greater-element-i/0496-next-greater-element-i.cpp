e07ca5ca69ef939ac5ec735ccb0e7d2e4d4c6058
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2025-12-31 17:52:17 +0530

    Time: 0 ms (100%), Space: 13 MB (13.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
-        for(int x :nums1 ){
-            int f=0 ,i;
-            for(i=0 ;i<nums2.size();i++){
-                if(nums2[i]==x){
-                    f=1 ;
-                    break ;
-                }}
-                if(f==1)
-                {
-                    for( ;i<nums2.size();i++){
-                        if(nums2[i]>x){
-                            v.push_back(nums2[i]);
-                            break ;
-                        }}
-                        if(i==nums2.size()){
-                            v.push_back(-1);
-                        }}
-                        else{
-                            v.push_back(-1);
-                        }
-                
-            }
-        
-        return v ;
+       stack<int>s;
+       map<int,int>m ;
+       for(int i=nums2.size()-1;i>=0;i--){
+        while(!s.empty()&& s.top()<nums2[i])s.pop();
+
+        if(s.empty()) m[nums2[i]]=-1;
+        else
+        m[nums2[i]]=s.top();
+        s.push(nums2[i]);
+       } 
+
+       vector<int>v;
+       for(int x:nums1)v.push_back(m[x]);
+       return v ;
     }
+
 };
