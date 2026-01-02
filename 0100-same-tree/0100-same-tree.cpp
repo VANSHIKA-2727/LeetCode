c9a528022abbdc289cc58f49cfd6e3f6f4782064
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2026-01-02 14:26:40 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.76%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,14 +12,14 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q== NULL){
+        if(p==NULL && q==NULL){
             return true ;
         }
-        else if( p== NULL || q==NULL){
+        else if(p==NULL|| q==NULL){
             return false ;
         }
-        else {
-            return ( p->val == q->val )&& isSameTree( p->left , q->left )&& isSameTree( p->right , q->right );
+        else{
+            return  p->val== q->val && isSameTree(p->left,q->left )&& isSameTree(p->right,q->right);
         }
     }
 };
