3561886f74383948aef17209395a2411b2daf8b9
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2026-01-05 15:40:15 +0530

    Time: 0 ms (100%), Space: 55.1 MB (31%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,44 @@
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>v;
+//         for(int i=0 ;i<n ;i++){
+//         if(nums[i]<0)continue ;
+//         else{
+//             v.push_back(nums[i]);
+//         }
+//         }
+//         int ans =0 ;
+//         for(int i=0 ;i<=n ;i++){
+//             ans = ans +i ;
+//         }
+//         int sum =0 ;
+//         for(auto x:v){
+//             sum=sum+x;
+//         }
+
+       
+//         return ans-sum ;
+//     }
+// };
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> present(n + 1, false);
+
+        for (int x : nums) {
+            if (x >= 1 && x <= n) {
+                present[x] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!present[i]) return i;
+        }
+
+        return n + 1;
+    }
+};
