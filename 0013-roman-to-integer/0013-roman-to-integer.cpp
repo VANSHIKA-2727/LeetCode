b4efcb422a347e86ca482a924139e7ab965da7cb
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2026-01-05 15:29:38 +0530

    Time: 4 ms (65.1%), Space: 10.1 MB (85.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-int roman (char c){
-    if(c=='I')return 1 ;
-    if(c=='V')return 5 ;
-    if(c=='X')return 10 ;
-    if(c=='L')return 50 ;
-    if(c=='C')return 100 ;
-    if(c=='D')return 500 ;
-    else 
-    return 1000 ;
+int roman (char ch ){
+    if(ch=='I')return 1;
+    if(ch=='V')return 5 ;
+     if(ch=='X')return 10 ;
+      if(ch=='L')return 50 ;
+       if(ch=='C')return 100 ;
+        if(ch=='D')return 500 ;
+        return 1000;
 
+    
 }
     int romanToInt(string s) {
-
         int ss =0 ;
-        for (int i = 0 ;i<s.length();i++){
-            if(i< s.length()-1  && roman (s[i])< roman (s[i+1])){
-                ss= ss-roman(s[i]);
-            }
+        for(int i=0 ;i<s.length() ;i++){
+            if(i<s.length()-1 && roman(s[i])<roman(s[i+1]))
+            ss= ss-roman(s[i]);
             else{
-                ss= ss+roman(s[i]);
+            ss=ss+roman(s[i]);
             }
         }
         return ss ;
-
     }
 };
