c478f95d48abce2e87aeee91a4664165139e11aa
Vanshika Dhawan <145150030+VANSHIKA-2727@users.noreply.github.com>
2026-01-18 22:38:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (79.61%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -13,11 +13,11 @@ public:
     }
 
     for(int i = 0; i < n; i++){
-        if(m1[s[i]] != m2[t[i]]){
-            return false;
-        }
-        m1[s[i]] = i + 1;
-        m2[t[i]] = i + 1;
+       if(m1[s[i]]==0 && m2[t[i]]==0){
+        m1[s[i]]=t[i];
+        m2[t[i]]=s[i];
+       }
+       if(m1[s[i]]!=t[i]||m2[t[i]]!=s[i])return false ;
     }
     return true;
     }
